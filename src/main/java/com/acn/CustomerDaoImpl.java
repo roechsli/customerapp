66d34691d552ba7381159187855bc12357e01fd7
roechsli <romanoech@gmail.com>
2019-04-04 14:48:07 +0200

    change info

--- src/main/java/com/acn/CustomerDaoImpl.java
+++ src/main/java/com/acn/CustomerDaoImpl.java
@@ -6,17 +6,6 @@ public class CustomerDaoImpl implements ICustomerDao {
 		// TODO Auto-generated constructor stub
 	}
 	
-//	customerService
-//	Dao
-//
-//	link with Spring
-//
-//	access database
-//
-//	config variables should come from a property file (property placeholderConfigurer if used with xml)
-//
-//	neither customerService nor ... should be created with "new" but rather with dependency injection
-//
-//	16.45 finished!
+
 
 }
--- src/main/java/com/acn/Infos.java
+++ src/main/java/com/acn/Infos.java
@@ -5,5 +5,16 @@ public class Infos {
 	public Infos() {
 		// TODO Auto-generated constructor stub
 	}
-
+//	customerService
+//	Dao
+//
+//	link with Spring
+//
+//	access database
+//
+//	config variables should come from a property file (property placeholderConfigurer if used with xml)
+//
+//	neither customerService nor ... should be created with "new" but rather with dependency injection
+//
+//	16.45 finished!
 }
