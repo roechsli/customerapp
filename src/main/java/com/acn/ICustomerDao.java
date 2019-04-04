7c69fb69b54ba04b398f98334278d6f5c0e61229
roechsli <romanoech@gmail.com>
2019-04-04 14:55:30 +0200

    create structure for customer interface

--- src/main/java/com/acn/ICustomerDao.java
+++ src/main/java/com/acn/ICustomerDao.java
@@ -1,5 +1,15 @@
 package com.acn;
 
-public interface ICustomerDao {
+import java.util.List;
 
+//import com.model.Customer;
+
+public interface ICustomerDao {
+//	List<Customer> getAllCustomers();
+//	List<Customer> getCustomerByName(String fname, String lname);
+//	Customer getCustomerById(Long id);
+//	
+//	void addCustomer(Customer cust);
+//	void changeCustomer(Customer cust);
+//	void deleteCustomer(Customer cust);
 }
