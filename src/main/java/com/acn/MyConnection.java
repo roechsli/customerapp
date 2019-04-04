35a94540cf77d952ddee69722f2748cb0d8fbd57
Marius <MariusP2507@users.noreply.github.com>
2019-04-04 14:34:34 +0200

    MyConnection class erstellt -toDo properties.prop!

--- src/main/java/com/acn/MyConnection.java
+++ src/main/java/com/acn/MyConnection.java
@@ -1,5 +1,52 @@
 package com.acn;
 
-public interface MyConnection {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class MyConnection {
+
+	private DataSource ds;
+	private Properties props = new Properties();
+
+	// Constructor - load the properties file, get the Data and set the Connection
+	public MyConnection() {
+		try {
+			
+			props.load(new FileReader(new File("properties.props")));
+
+			String url = props.getProperty("url");
+			String user = props.getProperty("dbuser");
+			String pass = props.getProperty("passwd");
+			String driverclass = props.getProperty("driverclass");
+			String poolsize = props.getProperty("poolsize");
+
+			BasicDataSource bds = new BasicDataSource();
+
+			bds.setUrl(url);
+			bds.setPassword(pass);
+			bds.setDriverClassName(driverclass);
+			bds.setUsername(user);
+			bds.setInitialSize(Integer.parseInt(poolsize));
+			ds = bds;
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public Connection getMyConnection() throws SQLException {
+		return ds.getConnection();
+	}
 }
