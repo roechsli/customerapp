a8c8374c1978022468e4cf7f57b88b03342ddda7
roechsli <romanoech@gmail.com>
2019-04-04 16:45:06 +0200

    finalizing tests

---
+++ src/main/java/com/acn/BeanConfig.java
@@ -0,0 +1,32 @@
+package com.acn;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.model.Customer;
+import com.model.ICustomer;
+
+@Configuration
+@ComponentScan
+public class BeanConfig {
+	@Bean(name="pers1")
+	public ICustomer customer1() {
+		return new Customer("Paul", "Panzer");
+	}
+	@Bean(name="pers2")
+	public ICustomer customer2() {
+		return new Customer("Friedrich", "Schimmel");
+	}
+	@Bean(name="pers3")
+	public ICustomer customer3() {
+		return new Customer("Arnold", "Fummel");
+	}
+	@Bean(name="pers4")
+	public ICustomer customer4() {
+		return new Customer("Peter", "Pilz");
+	}
+	@Bean(name="custDao")
+	public ICustomerDao customerDao() {
+		return new CustomerDaoImpl();
+	}
+}
--- src/test/java/com/acn/TestCustomerDao.java
+++ src/test/java/com/acn/TestCustomerDao.java
@@ -6,38 +6,72 @@ import java.util.List;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.model.Customer;
+import com.model.ICustomer;
 
 public class TestCustomerDao {
 	private static MyConnection myConn;
 	private static ICustomerDao custDao;
+	private static AnnotationConfigApplicationContext ctw;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		myConn = new MyConnection();
 		custDao = new CustomerDaoImpl();
+		ctw = new AnnotationConfigApplicationContext(BeanConfig.class);
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		myConn = null;
 		custDao = null;
+		((ConfigurableApplicationContext)ctw).close();
 	}
 
-	@Test
+	@Test @Ignore
 	public void testAddCustomer() {
 		Customer myCust = new Customer("Disslon", "Eklon");
 		custDao.addCustomer(myCust);
 		assertNotNull(custDao);
-//		List<Customer> myCustList = custDao.getCustomerByName("%lon","%lon");
-//		myCustList.forEach(System.out::println);
-//		assertNotNull(myCust.getId());
 	}
-	@Test
+	@Test @Ignore
 	public void testCustomerByName() {
 		List<Customer> myCustList = custDao.getCustomerByName("%lon","%lon");
 		myCustList.forEach(System.out::println);
+		assertNotNull(custDao);
+	}
+	@Test @Ignore
+	public void testBeansCustDao() {
+		ICustomerDao custDaoBean = (ICustomerDao) ctw.getBean("custDao");
+		custDaoBean.getAllCustomers().forEach(System.out::println);
+	}
+	@Test @Ignore
+	public void testBeansCusts() {
+		ICustomerDao custDaoBean = (ICustomerDao) ctw.getBean("custDao");
+		for (int i = 1; i<5; i++) {
+			String pers = "pers" + i;
+			ICustomer thisCust = (ICustomer) ctw.getBean(pers);
+			custDaoBean.addCustomer((Customer)thisCust);
+		}
+		List<Customer> myCustList = custDao.getAllCustomers();
+		myCustList.forEach(System.out::println);
+//		custDaoBean.getAllCustomers().forEach(System.out::println);
+//		System.out.println(custDaoBean.getAllCustomers());
+	}
+	@Test  
+	public void testDeleteCustomers() {
+		ICustomerDao custDaoBean = (ICustomerDao) ctw.getBean("custDao");
+		assertNotNull(custDaoBean.getAllCustomers());
+		List<Customer> myCustList = custDaoBean.getAllCustomers();
+		for (Customer cust : myCustList) {
+			custDaoBean.deleteCustomer(cust);
+		}
+		assertTrue(custDaoBean.getAllCustomers().isEmpty());
+		
 	}
 
 }
