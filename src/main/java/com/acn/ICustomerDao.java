f6ad1e409ec7c2fce27c80aa2bf75a4dc8457bb5
roechsli <romanoech@gmail.com>
2019-04-04 15:12:23 +0200

    trial

--- src/main/java/com/acn/ICustomerDao.java
+++ src/main/java/com/acn/ICustomerDao.java
@@ -2,14 +2,15 @@ package com.acn;
 
 import java.util.List;
 
-//import com.model.Customer;
+import com.model.Customer;
+
 
 public interface ICustomerDao {
-//	List<Customer> getAllCustomers();
-//	List<Customer> getCustomerByName(String fname, String lname);
-//	Customer getCustomerById(Long id);
-//	
-//	void addCustomer(Customer cust);
-//	void changeCustomer(Customer cust);
-//	void deleteCustomer(Customer cust);
+	List<Customer> getAllCustomers();
+	List<Customer> getCustomerByName(String fname, String lname);
+	Customer getCustomerById(Long id);
+	
+	void addCustomer(Customer cust);
+	void changeCustomer(Customer cust);
+	void deleteCustomer(Customer cust);
 }
--- src/main/java/com/acn/Service/ICustomerService.java
+++ src/main/java/com/acn/Service/ICustomerService.java
@@ -1,5 +1,5 @@
 package com.acn.Service;
 
 public interface ICustomerService {
-
+	
 }
