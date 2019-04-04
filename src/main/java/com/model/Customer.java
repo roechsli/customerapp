15aebc4c3eed1b255d545c12c131f93fe8be107f
monkeyblu <18556384+monkeyblu@users.noreply.github.com>
2019-04-04 14:55:30 +0200

    add customer class

---
+++ src/main/java/com/model/Customer.java
@@ -0,0 +1,13 @@
+package com.model;
+
+public class Customer {
+
+	public Customer() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	private Long id; //Wrapper Klassen verwenden wegen den vielen Vorteilen gegenueber den primitiven
+	private String firstName;
+	private String lastName;
+
+}
