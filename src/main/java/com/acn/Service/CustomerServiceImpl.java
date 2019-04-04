50ec2ef4804d3efdaddcd227d8fc16d36a9ee590
Marius <MariusP2507@users.noreply.github.com>
2019-04-04 14:57:11 +0200

    properties angepasst, service klassen erstellt und kommentare in der MyConnection erstellt

--- src/main/java/com/acn/Service/CustomerServiceImpl.java
+++ src/main/java/com/acn/Service/CustomerServiceImpl.java
@@ -1,9 +1,45 @@
 package com.acn.Service;
 
-public class CustomerServiceImpl implements ICustomerService {
+import java.util.List;
 
+import com.acn.CustomerDao;
+import com.model.Customer;
+
+public class CustomerServiceImpl implements CustomerService {
+	private CustomerDao custdao;
+	public CustomerServiceImpl(CustomerDao custdao) {
+		super();
+		this.custdao = custdao;
+	}
 	public CustomerServiceImpl() {
 		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public List<Customer> getAllCustomer() {
+		return custdao.getAllCustomer();
+	}
+	@Override
+	public List<Customer> getCustomerByName(String fname, String lname) {
+		return custdao.getCustomerByName(fname, lname);
+	}
+	@Override
+	public Customer getCustomerById(Long id) {
+		return custdao.getCustomerById(id);
+	}
+	@Override
+	public void addCustomer(Customer cust) {
+		custdao.addCustomer(cust);
+		
+	}
+	@Override
+	public void changeCustomer(Customer cust) {
+		custdao.changeCustomer(cust);
+		
+	}
+	@Override
+	public void deleteCustomer(Customer cust) {
+		custdao.deleteCustomer(cust);
+		
 	}
 
 }
--- src/main/java/com/acn/Service/ICustomerService.java
+++ src/main/java/com/acn/Service/ICustomerService.java
@@ -1,5 +1,16 @@
 package com.acn.Service;
 
+import java.util.List;
+
+import com.model.Customer;
+
 public interface ICustomerService {
+	List<Customer> getAllCustomer();
+	List<Customer> getCustomerByName(String fname, String lname);
+	Customer getCustomerById(Long id);
+
+	void addCustomer(Customer cust);
+	void changeCustomer(Customer cust);
+	void deleteCustomer(Customer cust);
 
 }
